4978821f6902aaca62488f5c02dedbdbc2483a22
Kristi Gribble <kgribble4083@gmail.com>
2020-01-17 21:34:59 -0600

    Minor Styling changes

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,15 +1,48 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  constructor() {
+    super();
+      this.state = {
+        githubuser: []
+  }
+}
 
+componentDidMount() {
+    fetch("https://api.github.com/users/rrawla2")
+    .then(response => response.json())
+    .then(users => {
+      console.log("kg: fetch: users", users)
+      this.setState({ ...this.state, githubuser: users})
+    })
+    .catch(err => console.log("Error on Fetch: ", err))
+  
+}
+
+  render() {
+    console.log("kg: render")
+  return (
+    <div className="container">
+      <header className="card header">
+        <div className="card">
+          <img className="card-img" src={this.state.githubuser.avatar_url} alt="avatar"></img>
+        
+          <h3  className="name">
+          {this.state.githubuser.name}
+          <p className="username">User Name: {this.state.githubuser.login}</p>
+          <p >Location: {this.state.githubuser.location}</p>
+          <p >Profile: {this.state.githubuser.url}</p> 
+          <p>Followers: {this.state.githubuser.followers}</p>
+          <p>Following: {this.state.githubuser.following}</p>
+          <p>Bio: {this.state.githubuser.bio}</p>
+          </h3>
+
+      </div>
       </header>
     </div>
-  );
-}
+  );//return
+}//render
+}//App
 
 export default App;
